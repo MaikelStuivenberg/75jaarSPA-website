fef7ebf76b07344e6fe321523e78e11e81ee81b7
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-04-27 12:35:28 +0200

    rotating images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = '75jaarspa';
+export class AppComponent implements OnInit, OnDestroy {
+  title: string = '75jaarspa';
+  currentBackgroundIndex: number = 0;
+  currentBackgroundImage: string = '../assets/fotos/2012CHOIR.jpeg';
+
+  images: string[] = [
+    '../assets/fotos/2012CHOIR.jpeg',
+    '../assets/fotos/2012TIMBRALS.jpeg',
+    '../assets/fotos/2015SPABAND.jpeg',
+    '../assets/fotos/2015ZANG.jpeg',
+    '../assets/fotos/2016MR.jpeg',
+  ];
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentBackgroundIndex += 1;
+      this.currentBackgroundImage = this.images[this.currentBackgroundIndex];
+      if(this.currentBackgroundIndex >= this.images.length - 1) this.currentBackgroundIndex = -1;
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+  }
 }
