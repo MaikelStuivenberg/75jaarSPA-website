ce0ae3699718edb5d5851b5b5c84f75233255eb1
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2023-04-27 12:26:07 +0200

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/notfound/notfound.component';
+import { ProgrammaComponent } from './pages/programma/programma.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "programma", component: ProgrammaComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/programma/programma.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'programma',
+  templateUrl: './programma.component.html',
+  styleUrls: ['./programma.component.css']
+})
+export class ProgrammaComponent {
+}
